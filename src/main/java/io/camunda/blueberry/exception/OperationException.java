62b028988721c81695c79527977ced680c812a00
Pierre-Yves <pierre-yves.monnet@laposte.net>
2025-02-16 14:04:48 -0800

    Add exception class

---
+++ src/main/java/io/camunda/blueberry/exception/OperationException.java
@@ -0,0 +1,23 @@
+package io.camunda.blueberry.exception;
+
+public class OperationException extends Exception {
+  private final String exceptionCode;
+  private final String explanation;
+
+  public OperationException(String exceptionCode, String explanation) {
+    this.exceptionCode = exceptionCode;
+    this.explanation = explanation;
+  }
+
+  public String getExceptionCode() {
+    return exceptionCode;
+  }
+
+  public String getExplanation() {
+    return explanation;
+  }
+
+  public String getHumanInformation() {
+    return "Code: [" + exceptionCode + "] " + explanation;
+  }
+}
